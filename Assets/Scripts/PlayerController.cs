8b847c5babc69fdb7c324554f49d8938c8dc3081
kyrie6510 <1936298733@qq.com>
2021-06-29 21:09:20 +0800

    增加任务跳跃受伤和敌人

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,10 +16,16 @@ public class PlayerController : MonoBehaviour
     public LayerMask ground;
 
     public int cherryCount=0;
+
+    public Text TxtCherryNum;
+    
     
     [SerializeField]
     private Animator anim;
     private Rigidbody2D rb;
+
+    public bool isHurt=false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -28,8 +36,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        if (!isHurt)
+        {
+            Movement();
+        }
         SwitchAnim();
+        
     }
 
     void Movement()
@@ -50,7 +62,7 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(facedDiection, 1, 1);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump")&&collider.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x, JumpForce * Time.deltaTime);
             anim.SetBool("jumping",true);
@@ -75,6 +87,20 @@ public class PlayerController : MonoBehaviour
             
             
         }
+
+        else if(isHurt)
+        {
+            anim.SetFloat("running",0);
+            anim.SetBool("hurt",true);
+            if (Mathf.Abs(rb.velocity.x) < 0.1f)
+            {
+                anim.SetBool("hurt",false);
+                anim.SetBool("idel",true);
+                isHurt = false;
+            }
+        }
+        
+        //判断是否接触地面
         else if (collider.IsTouchingLayers(ground))
         {
             anim.SetBool("falling",false);
@@ -89,8 +115,41 @@ public class PlayerController : MonoBehaviour
         if (other.tag == "Collection")
         {
             cherryCount += 1;
+            TxtCherryNum.text = "Cherry:" + cherryCount;
             Destroy(other.gameObject);
         }
+        
+    }
+    
+    //消灭敌人
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            if (anim.GetBool("falling"))
+            {
+                Destroy(other.gameObject);
+                rb.velocity = new Vector2(rb.velocity.x, JumpForce * Time.deltaTime);
+                anim.SetBool("jumping", true);
+            }
+            
+            else if (transform.position.x < other.gameObject.transform.position.x)
+            {
+                rb.velocity = new Vector2(-3, rb.velocity.y);
+                isHurt = true;
+            }
+            else if (transform.position.x > other.gameObject.transform.position.x)
+            {
+                rb.velocity = new Vector2(3, rb.velocity.y);
+                isHurt = true;
+            }
+
+            
+            
+        }
+
+      
+       
         
     }
 }
